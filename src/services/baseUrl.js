b196065cc28100c6e84c603b76a1ed81dbd5d527
Sumit Prajapati <sumitprajapati3115@gmail.com>
2026-04-23 13:16:06 +0530

    Update baseUrl.js

--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,3 +1,3 @@
 const baseUrl = "https://final-pos-b.onrender.com/api";
- const baseUrl = "srv-d7ksptm8bjmc73ddku4g/api";
+ //const baseUrl = "srv-d7ksptm8bjmc73ddku4g/api";
 export default baseUrl;
